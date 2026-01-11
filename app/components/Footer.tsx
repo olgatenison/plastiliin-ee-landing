7c11dcb831877b538bcdcd1f07597400bd58eb0f
olgatenison <88651744+olgatenison@users.noreply.github.com>
2026-01-11 13:05:27 +0200

    footer gradient

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default async function Footer() {
   const t = await getTranslations("Footer");
 
   return (
-    <footer className="bg-gradient-to-b from-[#9551c9]/20 to-[#9551c9]/50 text-gray-800">
+    <footer className="bg-linear-to-b from-[#9551c9]/20 to-[#9551c9]/50 text-gray-800">
       <div className="mx-auto max-w-7xl px-6  py-8 flex flex-col lg:flex-row items-center justify-between text-center gap-6">
         {/* Privacy policy link */}
         <Link
